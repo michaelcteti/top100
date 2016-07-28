4deb6f147c9caf4b8b5d8bf122500abb8319a335
Michael Teti <michael.teti@arcadiasolutions.com>
2016-07-28 15:03:54 -0400

    Include all states

--- public/scripts/top100.js
+++ public/scripts/top100.js
@@ -255,9 +255,6 @@ var CoursesList = React.createClass({
       if (this.props.list == 'public' && !course.public_rank) {
         return;
       }
-      console.log(this.props.list);
-      console.log(this.props.state);
-      console.log(course.state);
       if (this.props.list == 'state' && (!course.state_rank || this.props.state != course.state)){
         return;
       }
@@ -363,8 +360,57 @@ var CourseSelection = React.createClass({
         <input type="radio" name="list" ref="public" value="public" onChange={this.handleChange} /> Public
         <input type="radio" name="list" ref="state" value="state" onChange={this.handleChange} /> State
         <select name="stateSelect" ref="stateSelect" onChange={this.handleChange}>
-          <option value="MA">MA</option>
-          <option value="NY">NY</option>
+          <option value="AL">Alabama</option>
+          <option value="AK">Alaska</option>
+          <option value="AZ">Arizona</option>
+          <option value="AR">Arkansas</option>
+          <option value="CA">California</option>
+          <option value="CO">Colorado</option>
+          <option value="CT">Connecticut</option>
+          <option value="DE">Delaware</option>
+          <option value="DC">District Of Columbia</option>
+          <option value="FL">Florida</option>
+          <option value="GA">Georgia</option>
+          <option value="HI">Hawaii</option>
+          <option value="ID">Idaho</option>
+          <option value="IL">Illinois</option>
+          <option value="IN">Indiana</option>
+          <option value="IA">Iowa</option>
+          <option value="KS">Kansas</option>
+          <option value="KY">Kentucky</option>
+          <option value="LA">Louisiana</option>
+          <option value="ME">Maine</option>
+          <option value="MD">Maryland</option>
+          <option value="MA">Massachusetts</option>
+          <option value="MI">Michigan</option>
+          <option value="MN">Minnesota</option>
+          <option value="MS">Mississippi</option>
+          <option value="MO">Missouri</option>
+          <option value="MT">Montana</option>
+          <option value="NE">Nebraska</option>
+          <option value="NV">Nevada</option>
+          <option value="NH">New Hampshire</option>
+          <option value="NJ">New Jersey</option>
+          <option value="NM">New Mexico</option>
+          <option value="NY">New York</option>
+          <option value="NC">North Carolina</option>
+          <option value="ND">North Dakota</option>
+          <option value="OH">Ohio</option>
+          <option value="OK">Oklahoma</option>
+          <option value="OR">Oregon</option>
+          <option value="PA">Pennsylvania</option>
+          <option value="RI">Rhode Island</option>
+          <option value="SC">South Carolina</option>
+          <option value="SD">South Dakota</option>
+          <option value="TN">Tennessee</option>
+          <option value="TX">Texas</option>
+          <option value="UT">Utah</option>
+          <option value="VT">Vermont</option>
+          <option value="VA">Virginia</option>
+          <option value="WA">Washington</option>
+          <option value="WV">West Virginia</option>
+          <option value="WI">Wisconsin</option>
+          <option value="WY">Wyoming</option>
         </select>
       </form>
     )
